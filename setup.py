3f9b250f2e28407582e2f1c5cd7b7a6a590a0bd4
Sam Wyatt <sam.wyatt@10gen.com>
2015-05-14 16:23:05 -0400

    Narrowed the setup requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup
 
 requires = [
     "Flask==0.10.1",
-    "Jinja2==2.7.3",
-    "MarkupSafe==0.23",
-    "Werkzeug==0.10.4",
-    "itsdangerous==0.24",
-    "wsgiref==0.1.2",
     "PyYAML==3.11",
     "pymongo==3.0.1"
 ]
